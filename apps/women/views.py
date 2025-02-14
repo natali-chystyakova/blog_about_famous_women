f27a7ef541bbc59969ad9d4b638ece8a46c2d10a
natali <natali.dnp.ua@gmail.com>
2025-02-14 18:39:01 +0200

    add class WomenAPIView, WomenAPIListPagination, WomenAPIList, WomenAPIUpdate, WomenAPIDestroyView and imports to them

--- apps/women/views.py
+++ apps/women/views.py
@@ -1,12 +1,31 @@
+# from pickle import FALSE
+
 from django.shortcuts import redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse_lazy
+
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+
 from .models import Woman, Category
 from .forms import AddPostForm, ContactForm
+from .permissions import IsAdminOrReadOnly
 from .utils import DataMixin
 from django.views.generic import ListView, DetailView, CreateView, FormView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from rest_framework import generics
+from .serializers import WomenSerializer
+
+# from django.forms import model_to_dict
+# from django.utils.text import slugify
 
 
 class WomenHome(DataMixin, ListView):
@@ -102,3 +121,105 @@ class WomenCategory(DataMixin, ListView):
         c_def = self.get_user_context(title="Категория-" + str(c.name), cat_selected=c.pk)
         context = dict(list(context.items()) + list(c_def.items()))
         return context
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Woman.objects.all()  # берем все записи
+#     serializer_class = WomenSerializer
+
+
+# класс для пагинации
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    # page_query_param = "page_size"
+    page_size_query_param = "page_size"
+    max_page_size = 2
+
+
+# в этих 3 классах повторяющийся код, без ViewSet
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Woman.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
+
+
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Woman.objects.all()
+    serializer_class = WomenSerializer
+    # permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    # authentication_classes = (TokenAuthentication, ) # кто получает доступ по токену
+
+
+class WomenAPIDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Woman.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+# class WomenViewSet(viewsets.ModelViewSet):
+#     #queryset = Woman.objects.all()
+#     serializer_class = WomenSerializer
+#
+#     def get_queryset(self):
+#         pk = self.kwargs.get("pk")
+#         if not pk:
+#             return Woman.objects.all()[:3]
+#         return Woman.objects.filter(pk=pk)
+#
+#
+#
+#     @action(methods=["get"], detail=True)
+#     def category(self, request, pk = None):
+#         #cats = Category.objects.all()
+#         cats = Category.objects.get(pk=pk)
+#         #return Response({"cats": [c.name for c in cats]})
+#         return Response({"cats": cats.name})
+
+
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Woman.objects.all()
+#         # return Response({"title": "Angelina Jolie"})
+#         return Response({"posts": WomenSerializer(w, many=True).data})
+#
+#
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#
+#
+#         # post_new = Woman.objects.create(
+#         #     title = request.data["title"],
+#         #     content = request.data["content"],
+#         #     cat_id = request.data["cat_id"],
+#         # )
+#         # return Response({ "post": WomenSerializer(post_new).data})
+#         return Response({"post": serializer.data})
+#
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk :
+#             return Response({"error": "Method PUT not allowed"})
+#         try:
+#             instance = Woman.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+#         serializer = WomenSerializer(data = request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"post": serializer.data})
+#
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method DELETE not allowed"}, status=400)
+#
+#         try:
+#             woman = Woman.objects.get(pk=pk)
+#             woman.delete()
+#             return Response({"message": f"Post {pk} deleted successfully"}, status=204)
+#         except Woman.DoesNotExist:
+#             return Response({"error": "Object does not exist"}, status=404)
