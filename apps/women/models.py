b9e777c3d2eb93874ba3764e14e0e8213cdf97ff
natali <natali.dnp.ua@gmail.com>
2025-02-14 18:14:42 +0200

    add user to class Woman, fix field photo and slug

--- apps/women/models.py
+++ apps/women/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Woman(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    # slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    slug = models.SlugField(max_length=255, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name="Фото")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, null=True, verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
     cat = models.ForeignKey("Category", on_delete=models.PROTECT, verbose_name="Категория")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь", default=1)
 
     def __str__(self):
         return self.title
